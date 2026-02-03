6ff3593bb61b1ce1adc5479c9fc7d9735b469745
TomasTamburrino <tomas.tamburrino@davinci.edu.ar>
2026-02-03 19:41:55 -0300

    Solitario

---
+++ Assets/Assets/Scripts/Games/Solitario/UpdateSprite.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSprite : MonoBehaviour
+{
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private SpriteRenderer spriteRenderer;
+    private Selectable selectable;
+    private Solitario solitaire;
+    private UserInput userInput;
+
+
+
+    void Start()
+    {
+        List<string> deck = Solitario.GenerateDeck();
+        solitaire = FindObjectOfType<Solitario>();
+        userInput = FindObjectOfType<UserInput>();
+
+        int i = 0;
+        foreach (string card in deck)
+        {
+            if (this.name == card)
+            {
+                cardFace = solitaire.cardFaces[i];
+                break;
+            }
+            i++;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<Selectable>();
+    }
+
+    void Update()
+    {
+        if (selectable.faceUp == true)
+        {
+            spriteRenderer.sprite = cardFace;
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+
+        if (userInput.slot1)
+        {
+
+            if (name == userInput.slot1.name)
+            {
+                spriteRenderer.color = Color.yellow;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+            }
+        }
+    }
+}
